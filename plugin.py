8efeefa98721691432ace9beeb437871840da5f3
doki <27952620+doopa75@users.noreply.github.com>
2020-02-15 16:40:40 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -72,7 +72,7 @@ class BasePlugin:
     def onHeartbeat(self):
         self.runInterval -=1;
         
-        if (self.runInterval <= 0) and (Parameters["Mode2"] == "tri"):
+        if self.runInterval <= 0 # and (Parameters["Mode2"] == "tri"):
             DCVreg = 30771
             # Get data from SMA
             try:
@@ -136,12 +136,12 @@ class BasePlugin:
 
             self.runInterval = int(Parameters["Mode3"]) * 6
 
-        elif (self.runInterval <= 0) and (Parameters["Mode2"] == "boy"):
-            Domoticz.Log("Your inverter is SMA Sunny Boy")
-            self.runInterval = int(Parameters["Mode3"]) * 6
-        else:
-            Domoticz.Log("Your inverter is unknow")
-            self.runInterval = int(Parameters["Mode3"]) * 6
+        #elif (self.runInterval <= 0) and (Parameters["Mode2"] == "boy"):
+            #Domoticz.Log("Your inverter is SMA Sunny Boy")
+            #self.runInterval = int(Parameters["Mode3"]) * 6
+        #else:
+            #Domoticz.Log("Your inverter is unknow")
+            #self.runInterval = int(Parameters["Mode3"]) * 6
 
 global _plugin
 _plugin = BasePlugin()
